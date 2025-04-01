f713c0b0f6e1113316b61a2acf0fd5ceb23fda3a
METEL <89406687+MetelOfficial@users.noreply.github.com>
2025-04-01 13:13:44 +0300

    Add files via upload

---
+++ mining/Start-Salvium.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig -a rx/0 --url "sal.kryptex.network:7777" --user SaLvsCNbjABPoCfjoVjytHf5Jqfd28NTD9kmkYUfSG7D2uzksP1TbNeDZB7ibriXB7D5M3YxfNZ7ER9amRZw25hQBR8kgDrhLRf/MyFirstRig -p x -k
